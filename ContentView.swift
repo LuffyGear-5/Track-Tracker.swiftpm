36889814f5cee36ffebe3ad46e628aa628480023
Aadi Shha <ashah8399@stu.d214.org>
2025-04-16 08:39:05 -0700

    Made color scheme match logo

--- ContentView.swift
+++ ContentView.swift
@@ -29,6 +29,8 @@ struct ContentView: View {
                 
                 
             }
+            .accentColor(.orange)
+            .foregroundStyle(.orange)
             
         }
     }
