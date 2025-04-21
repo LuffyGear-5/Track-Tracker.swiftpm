1e67c196cfe5216197e3f455c6aa12a2a912d6ae
Aadi Shha <ashah8399@stu.d214.org>
2025-04-21 08:47:53 -0700

    Made button for magenta preset

--- Appearance.swift
+++ Appearance.swift
@@ -1,8 +1,17 @@
 import SwiftUI
 
 struct Appearance: View {
+    @State var customColor: Color = Color(red: 1.0, green: 0.0, blue: 1.0)
     var body: some View {
-        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+        Button{
+            
+        }label: {
+            Text("Magenta")
+                .frame(width: 110, height: 50)
+                .background(customColor)
+                .foregroundStyle(.black)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+        }
     }
 }
 
