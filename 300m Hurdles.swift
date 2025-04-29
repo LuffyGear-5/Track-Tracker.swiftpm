9e68f60bbdcf93e08908c7b4332011a15a68ff2a
Aadi Shha <ashah8399@stu.d214.org>
2025-04-29 08:54:40 -0700

    Copy pasted ranking system

--- 300m Hurdles.swift
+++ 300m Hurdles.swift
@@ -3,23 +3,39 @@ import SwiftUI
 
 
 struct Hurdles300m: View {
-    @State var time : [String] = []
-    @State var text1 = ""
+    @State private var inputText: String = ""
+    @State private var numbers: [Double] = []
+
     var body: some View {
-        VStack{
-            List(time,id: \.self){ times in
-                Text(times)
-            }
+        VStack {
+                
+                List {
+                    ForEach(sortedNumbers, id: \.self) { number in
+                        Text("\(number, specifier: "%.2f")")
+                    }
+                    
+                }
             HStack{
-                TextField("Enter time", text: $text1)
-                    .textFieldStyle(.roundedBorder)
-                Button{
-                    time.append(text1)
-                    text1 = ""
-                } label: {
-                    Image(systemName: "plus.circle")
+                TextField("Enter your time", text: $inputText)
+                    .keyboardType(.numberPad)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                Button(action: addNumber) {
+                    Text("Add")
+                        .padding(.horizontal)
                 }
             }
+        }
+    }
+    
+    var sortedNumbers: [Double] {
+        numbers.sorted(by: <)
+    }
+
+    func addNumber() {
+        if let number = Double(inputText) {
+            numbers.append(number)
+            inputText = ""
         }
     }
 }
--- Long Jump.swift
+++ Long Jump.swift
@@ -3,9 +3,39 @@ import SwiftUI
 
 
 struct LongJump: View {
-    @State var text13 = ""
+    @State private var inputText: String = ""
+    @State private var numbers: [Double] = []
+
     var body: some View {
-        TextField("Add your distance", text: $text13)
+        VStack {
+                
+                List {
+                    ForEach(sortedNumbers, id: \.self) { number in
+                        Text("\(number, specifier: "%.2f")")
+                    }
+                    
+                }
+            HStack{
+                TextField("Enter your Distance", text: $inputText)
+                    .keyboardType(.numberPad)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                Button(action: addNumber) {
+                    Text("Add")
+                        .padding(.horizontal)
+                }
+            }
+        }
+    }
+    
+    var sortedNumbers: [Double] {
+        numbers.sorted(by: <)
+    }
+
+    func addNumber() {
+        if let number = Double(inputText) {
+            numbers.append(number)
+            inputText = ""
+        }
     }
 }
-
