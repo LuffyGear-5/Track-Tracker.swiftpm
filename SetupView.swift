dc417fff117984b2a7ee2f0a39628697464a515d
Dmytro Shumyk <dshumyk8405@stu.d214.org>
2025-05-13 08:38:39 -0700

    NavigationLink was added

--- SetupView.swift
+++ SetupView.swift
@@ -1,8 +1,26 @@
 import SwiftUI
 
 struct SetupView: View {
+    
+    @Binding var alertColor : Color
     var body: some View {
-        Text("Under Construction")
+        NavigationStack {
+            VStack {
+                NavigationStack{
+                    NavigationLink("Go to ContentView", destination: ContentView( alertColor: $alertColor))
+                        .font(.headline)
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                    
+                    
+                    
+                }
+
+            }
+        }
+       
     }
 }
 
