464e1d851387322c9e941683772a726045f38c5d
Dominic Galan <dgalan8141@stu.d214.org>
2025-05-23 10:48:15 -0500

    Added animations for buttons in future

--- BlankView.swift
+++ BlankView.swift
@@ -1,11 +1,26 @@
 import SwiftUI
 
+
+
 struct BlankView: View {
-    var body: some View {
-    
-        ZStack{
-            
+    @State private var isScaledUp = false
+
+        var body: some View {
+            VStack {
+                Circle()
+                    .fill(Color.orange)
+                    .frame(width: 100, height: 100)
+                    .scaleEffect(isScaledUp ? 2.0 : 1.0)  // scale up or normal
+                    .animation(.easeInOut(duration: 0.5), value: isScaledUp)
+
+                Button("Toggle Scale") {
+                    isScaledUp.toggle()
+                }
+                .padding()
+            }
         }
-        
     }
+
+#Preview {
+    BlankView()
 }
